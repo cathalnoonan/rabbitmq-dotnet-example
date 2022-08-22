c9801adc91fb05576c093ac4672710644eb16f21
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2022-08-22 21:32:46 +0100

    Added producer and consumer (console apps)

---
+++ consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace consumer;
+
+public class Program
+{
+    private const string QUEUE_NAME = "hello";
+
+    public static void Main(params string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(
+            queue: QUEUE_NAME,
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = System.Text.Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [x] Received {0}", message);
+        };
+
+        channel.BasicConsume(queue: QUEUE_NAME, autoAck: true, consumer: consumer);
+
+        // Only keep the console open if no arguments are passed.
+        // Include this for CI purposes.
+        if (args.Length == 0)
+        {
+            System.Console.WriteLine("Press enter to close...");
+            System.Console.ReadLine();
+        }
+
+        channel.Close();
+        connection.Close();
+    }
+}
---
+++ producer/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+
+namespace producer;
+
+public class Program
+{
+    private const string QUEUE_NAME = "hello";
+
+    public static void Main(params string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(
+            queue: QUEUE_NAME,
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        var message = GetMessage(args);
+        var body = System.Text.Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(
+            exchange: "",
+            routingKey: QUEUE_NAME,
+            basicProperties: null,
+            body: body
+        );
+
+        System.Console.WriteLine($" [x] Sent message: '{message}' to the queue: '{QUEUE_NAME}'.");
+
+        channel.Close();
+        connection.Close();
+    }
+
+    private static string GetMessage(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            return string.Join(" ", args);
+        }
+
+        // As a default, send hard-coded message to the queue.
+        return "Hello, World!";
+    }
+}
